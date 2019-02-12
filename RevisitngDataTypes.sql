72ade1087522fb666dd7b9b1d40cc0535121e531
Tushar <gupta.tus@husky.neu.edu>
2019-02-12 06:23:41 -0500

    Revisiting Datatypes started

---
+++ RevisitngDataTypes.sql
@@ -0,0 +1,43 @@
+CREATE TABLE dogs (name CHAR(5), breed VARCHAR(10));
+ 
+INSERT INTO dogs (name, breed) VALUES ('bob', 'beagle');
+ 
+INSERT INTO dogs (name, breed) VALUES ('robby', 'corgi');
+ 
+INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retriever');
+ 
+SELECT * FROM dogs;
+ 
+INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retrievesadfdsafdasfsafr');
+ 
+SELECT * FROM dogs;
+
+
+CREATE TABLE items(price DECIMAL(5,2));
+ 
+INSERT INTO items(price) VALUES(7);
+ 
+INSERT INTO items(price) VALUES(7987654);
+ 
+INSERT INTO items(price) VALUES(34.88);
+ 
+INSERT INTO items(price) VALUES(298.9999);
+ 
+INSERT INTO items(price) VALUES(1.9999);
+ 
+SELECT * FROM items;
+
+CREATE TABLE thingies (price FLOAT);
+ 
+INSERT INTO thingies(price) VALUES (88.45);
+ 
+SELECT * FROM thingies;
+ 
+INSERT INTO thingies(price) VALUES (8877.45);
+ 
+SELECT * FROM thingies;
+ 
+INSERT INTO thingies(price) VALUES (8877665544.45);
+ 
+SELECT * FROM thingies;
+
